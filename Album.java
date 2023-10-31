1296b0777f03278652e32950e13e98e06377b669
Rifat <rifat0531m@gmail.com>
2023-11-01 00:00:08 +0300

    Album class updated

--- Album.java
+++ Album.java
@@ -1,54 +1,25 @@
-import java.util.List;
-
-public class Album implements IMusicCollection{
+public class Album {
     private String title;
-    private List<Song> listSongs;
-    private Artist artist;
+    private String description;
 
     public Album(String title) {
         this.title = title;
     }
 
-    public Album(String title, Artist artist) {
-        this.title = title;
-        this.artist = artist;
-    }
-
-    public Artist getArtist() {
-        return this.artist;
-    }
-
-    public void setArtist(Artist artist) {
-        this.artist = artist;
-    }    
-
-    @Override
     public String getTitle() {
         return this.title;
     }
 
-    @Override
     public void setTitle(String title) {
         this.title = title;
     }
 
-    @Override
-    public List<Song> getListSongs() {
-        return this.listSongs;
+    public String getDescription() {
+        return this.description;
     }
 
-    @Override
-    public void setListSongs(List<Song> listSongs) {
-        this.listSongs = listSongs;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    @Override
-    public void addSong(Song song) {
-        this.listSongs.add(song);
-    }
-
-    @Override
-    public void removeSong(Song song) {
-        this.listSongs.remove(song);
-    }
 }
