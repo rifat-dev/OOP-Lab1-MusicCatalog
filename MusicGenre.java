517427a713112b6f212fa3a92a8fa85708478c92
Rifat <rifat0531m@gmail.com>
2023-10-31 10:02:36 +0300

    MusicGenre.java

--- MusicGenre.java
+++ MusicGenre.java
@@ -1,3 +1,15 @@
 public class MusicGenre {
-    
+    private String name;
+
+    public MusicGenre(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
