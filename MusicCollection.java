37f2f5fdad5f7b8b9f3e97d738a9240deedc24ba
Rifat <rifat0531m@gmail.com>
2023-11-01 00:01:23 +0300

    MusicCollection class updated

--- MusicCollection.java
+++ MusicCollection.java
@@ -1,41 +1,53 @@
+import java.util.ArrayList;
 import java.util.List;
 
-public class MusicCollection implements IMusicCollection {
-    private String title;
-    private List<Song> listSongs;
+public class MusicCollection {
+    private List<Song> listSongs = new ArrayList<>();
 
-    public MusicCollection(String title) {
-        this.title = title;
+    public MusicCollection() {
+
     }
 
-    @Override
-    public String getTitle() {
-        return this.title;
-    }
-
-    @Override
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public List<Song> getListSongs() {
-        return this.listSongs;
-    }
-
-    @Override
-    public void setListSongs(List<Song> listSongs) {
-        this.listSongs = listSongs;
-    }
-
-    @Override
     public void addSong(Song song) {
         this.listSongs.add(song);
     }
 
-    @Override
     public void removeSong(Song song) {
         this.listSongs.remove(song);
+    }
+
+    // NB (для себя) что если использовать отдельный класс для поиска и применить паттерн стратегию с разными реализациями поиска
+    public List<Song> findByName(String name) {
+        List<Song> result = new ArrayList<>();
+        for (Song item: listSongs) {
+            if (item.getName().toLowerCase().contains(name.toLowerCase())) {
+                result.add(item);
+            }
+        }
+
+        return result;
+    }
+
+    public List<Song> findByGenreName(String name) {
+        List<Song> result = new ArrayList<>();
+        for (Song item: listSongs) {
+            if (item.getGenre().getName().toLowerCase().contains(name.toLowerCase())) {
+                result.add(item);
+            }
+        }
+
+        return result;
+    }
+
+    public List<Song> findByArtistNickName(String nickName) {
+        List<Song> result = new ArrayList<>();
+        for (Song item: listSongs) {
+            if (item.getArtist().getArtistMetaData().getNickname().toLowerCase().contains(nickName.toLowerCase())) {
+                result.add(item);
+            }
+        }
+
+        return result;
     }
 
 }
