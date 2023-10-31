c85761a9b00b84e524deeb91a02866d49c23d996
Rifat <rifat0531m@gmail.com>
2023-10-31 14:11:37 +0300

    some refactoring

--- MusicCollection.java
+++ MusicCollection.java
@@ -1,6 +1,6 @@
 import java.util.List;
 
-public class MusicCollection {
+public class MusicCollection implements IMusicCollection {
     private String title;
     private List<Song> listSongs;
 
@@ -8,26 +8,32 @@ public class MusicCollection {
         this.title = title;
     }
 
+    @Override
     public String getTitle() {
         return this.title;
     }
 
+    @Override
     public void setTitle(String title) {
         this.title = title;
     }
 
+    @Override
     public List<Song> getListSongs() {
         return this.listSongs;
     }
 
+    @Override
     public void setListSongs(List<Song> listSongs) {
         this.listSongs = listSongs;
     }
 
+    @Override
     public void addSong(Song song) {
         this.listSongs.add(song);
     }
 
+    @Override
     public void removeSong(Song song) {
         this.listSongs.remove(song);
     }
