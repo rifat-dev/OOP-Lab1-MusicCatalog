e8155988f8e5d9f2701ce2b7ca0887c78cf8bb4a
Rifat <rifat0531m@gmail.com>
2023-10-31 10:28:01 +0300

    Artist classes

--- Artist.java
+++ Artist.java
@@ -1,3 +1,17 @@
 public class Artist {
+    private ArtistMetaData artistMetaData;
+
+    public Artist(ArtistMetaData artistMetaData) {
+        this.artistMetaData = artistMetaData;
+    }
+
+
+    public ArtistMetaData getArtistMetaData() {
+        return this.artistMetaData;
+    }
+
+    public void setArtistMetaData(ArtistMetaData artistMetaData) {
+        this.artistMetaData = artistMetaData;
+    }
     
 }
