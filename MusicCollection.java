8dba269d57662daf810afc7063705d3f4007fe3b
Rifat <rifat0531m@gmail.com>
2023-10-31 10:46:54 +0300

    MusicCollection

--- MusicCollection.java
+++ MusicCollection.java
@@ -1,3 +1,35 @@
+import java.util.List;
+
 public class MusicCollection {
-    
+    private String title;
+    private List<Song> listSongs;
+
+    public MusicCollection(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Song> getListSongs() {
+        return this.listSongs;
+    }
+
+    public void setListSongs(List<Song> listSongs) {
+        this.listSongs = listSongs;
+    }
+
+    public void addSong(Song song) {
+        this.listSongs.add(song);
+    }
+
+    public void removeSong(Song song) {
+        this.listSongs.remove(song);
+    }
+
 }
